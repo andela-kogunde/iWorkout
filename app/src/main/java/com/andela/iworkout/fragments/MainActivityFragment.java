4afa64a570b4b86f9bedeb62713657429db59b96
andela <kehinde.ogunde@andela.com>
2016-03-11 15:14:07 +0100

    Add initializeComponent to MainActivityFragment class

--- app/src/main/java/com/andela/iworkout/fragments/MainActivityFragment.java
+++ app/src/main/java/com/andela/iworkout/fragments/MainActivityFragment.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.andela.iworkout.R;
+
+import me.itangqi.waveloadingview.WaveLoadingView;
 
 public class MainActivityFragment extends Fragment {
 
@@ -17,5 +20,21 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initializeComponent(view);
+    }
+
+    private void initializeComponent(View view) {
+        WaveLoadingView totalPushUps = (WaveLoadingView) view.findViewById(R.id.totalPushUps);
+        WaveLoadingView todaysPushUps = (WaveLoadingView) view.findViewById(R.id.todaysPushUps);
+        WaveLoadingView caloriesBurned = (WaveLoadingView) view.findViewById(R.id.caloriesBurned);
+        WaveLoadingView energyGained = (WaveLoadingView) view.findViewById(R.id.energyGained);
+
+
     }
 }
