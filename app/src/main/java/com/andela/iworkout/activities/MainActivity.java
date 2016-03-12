87e2d6a7385df7e2a414491118770365c6e09a18
andela <kehinde.ogunde@andela.com>
2016-03-12 23:53:59 +0100

    Add onclick to main activity resource

--- app/src/main/java/com/andela/iworkout/activities/MainActivity.java
+++ app/src/main/java/com/andela/iworkout/activities/MainActivity.java
@@ -1,13 +1,11 @@
 package com.andela.iworkout.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.andela.iworkout.R;
 
@@ -40,5 +38,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void launchWorkout(View view) {
+        
+    }
+
+    public void launchAnalytics(View view) {
+
     }
 }
