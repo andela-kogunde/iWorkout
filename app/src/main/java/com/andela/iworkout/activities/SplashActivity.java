999b641193b37fba35b3227b3fc907fe54ea322a
andela <kehinde.ogunde@andela.com>
2016-03-22 05:57:41 +0100

    Refactor SplashActivity class

--- app/src/main/java/com/andela/iworkout/activities/SplashActivity.java
+++ app/src/main/java/com/andela/iworkout/activities/SplashActivity.java
@@ -1,17 +1,35 @@
 package com.andela.iworkout.activities;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.andela.iworkout.utilities.Launcher;
+import com.andela.iworkout.utilities.MsgBox;
+import com.andela.iworkout.notifications.PushUpNotificationBuilder;
+import com.andela.iworkout.notifications.PushUpNotificationManager;
+import com.andela.iworkout.utilities.Settings;
 
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        managePushUpNotifications(this);
+
         Launcher.launch(this, MainActivity.class);
         finish();
+    }
+
+    private void managePushUpNotifications(Context context) {
+        PushUpNotificationManager notificationManager = new PushUpNotificationManager(context);
+        notificationManager.cancelPushUpNotifications();
+
+        if (Settings.getNotificationMode(context)) {
+            notificationManager.setPushUpNotifications(
+                    PushUpNotificationBuilder.getNotification(context));
+            MsgBox.show(context, "Pushup been created");
+        }
     }
 }
