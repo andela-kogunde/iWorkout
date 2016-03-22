1a3637e682fc327cf1b033e9dec77b7849d6b612
andela <kehinde.ogunde@andela.com>
2016-03-22 06:00:11 +0100

    Add methods to get Notifications preference values

--- app/src/main/java/com/andela/iworkout/utilities/Settings.java
+++ app/src/main/java/com/andela/iworkout/utilities/Settings.java
@@ -3,6 +3,7 @@ package com.andela.iworkout.utilities;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.preference.PreferenceManager;
 
 public class Settings {
@@ -24,5 +25,32 @@ public class Settings {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
         int intValue = Integer.valueOf(preferences.getString("set_number_of_pushup", ""));
         return (intValue < 0) ? 0 : intValue;
+    }
+
+    public static boolean getNotificationMode(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getBoolean("notifications_pushup", false);
+    }
+
+    public static Uri getRingtone(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String value = preferences.getString("notifications_pushup_ringtone", "");
+        return value.isEmpty() ? null : Uri.parse(value);
+    }
+
+    public static boolean getVibrate(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getBoolean("notifications_pushup_vibrate", false);
+    }
+
+    public static int getReminder(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String value = preferences.getString("pushup_reminder", "");
+        return value.isEmpty() ? -1 : Integer.valueOf(value);
+    }
+
+    public static String getAlarmTime(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getString("alarm_time", "");
     }
 }
